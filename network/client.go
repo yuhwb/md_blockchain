e205a3a9a4ff437744d6baec3e00bcc657bdb5e0
yuhaiwei <yuhaiwei_bj@126.com>
2018-04-27 23:37:55 +0800

    ceshi

--- network/client.go
+++ network/client.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"net"
 	"os"
+	"bytes"
+	"encoding/binary"
+	"strconv"
+	"io"
+	"fmt"
 )
 
 func main() {
+	ch := make(chan int)
 	conn, err := net.Dial("tcp", "localhost:9000")
 	if err != nil {
 		log.Fatal(1,err)
 		os.Exit(1)
 	}
 	defer conn.Close()
+
 
 	go func() {
 		for {
@@ -22,13 +28,37 @@ func main() {
 			if err != nil {
 				log.Printf("error:%v", err)
 			}
-			log.Print(string(data))
+			fmt.Println(string(data))
 		}
 	}()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		conn.Write(scanner.Bytes())
+	for i := 0; i < 100; i++ {
+		go doWrite(conn , 1)
+	}
+	for i := 0; i < 100; i++ {
+		go doWrite(conn , 2)
+	}
+	for i := 0; i < 100; i++ {
+		go doWrite(conn , 3)
+	}
+	for i := 0; i < 100; i++ {
+		go doWrite(conn , 4)
+	}
+	for i := 0; i < 100; i++ {
+		go doWrite(conn , 5)
 	}
 
+	<-ch
+
+}
+
+func doWrite(conn net.Conn , n int)  {
+	s := strconv.Itoa(n) + "\n"
+	io.WriteString(conn , s)
+}
+
+func IntToBytes(n int) []byte{
+	buf := bytes.NewBuffer(nil)
+	binary.Write(buf , binary.BigEndian , n)
+	return buf.Bytes()
 }
