b320be2aad03913c75dc6f8e1a03d27b21d5ffc4
yuhaiwei <yuhaiwei_bj@126.com>
2018-04-28 20:51:36 +0800

    ceshi

--- proof-work/main.go
+++ proof-work/main.go
@@ -187,8 +187,8 @@ func main() {
 	server := &http.Server{
 		Addr:           ":8080",
 		Handler:        nil,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		//ReadTimeout:    10 * time.Second,
+		//WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 	if err := server.ListenAndServe(); err != nil {
